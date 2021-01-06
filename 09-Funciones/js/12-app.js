b6fa0f20a8f035414f04a07f9b135210af546d16
Rodrigo Jesus Sanchez <rodrigojesus-sanchez@aj.com.py>
2021-01-06 17:34:00 -0300

    Funciones de flecha, iterando forEach map

--- 09-Funciones/js/12-app.js
+++ 09-Funciones/js/12-app.js
@@ -17,8 +17,15 @@ const carrito=[
         precio: 250
     }
 ]
+carrito.forEach(producto=>console.log(`${producto.nombre}`));
+
+const nuevoCarrito2=carrito.forEach(producto=>`${producto.nombre}`);
 
 //a diferencia de forEach, map crea un nuevo arreglo
 const nuevoCarrito=carrito.map(producto=>{
    return `${producto.nombre}`; 
 });
+
+const nuevoCarrito3=carrito.map(producto=>`${producto.nombre}`);
+
+console.log(nuevoCarrito);
