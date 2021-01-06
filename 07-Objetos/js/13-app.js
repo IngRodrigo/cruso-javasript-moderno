0b1ac87dbd2ac1f1c285392b30a29b6d11c92f70
Rodrigo Jesus Sanchez <rodrigojesus-sanchez@aj.com.py>
2021-01-06 15:39:59 -0300

    Object key, values, entries

---
+++ 07-Objetos/js/13-app.js
@@ -0,0 +1,12 @@
+//Object keys, values, entries
+const producto={
+    nombre:"Monitor de 20 pulgadas",
+    precio:200,
+    disponible:true
+}
+//obtener las claves del objeto, 'Nombre de las propiedades'
+console.log(Object.keys(producto));
+//obtener los valores
+console.log(Object.values(producto));
+//devuelve en pares 'calve y valor'
+console.log(Object.entries(producto));
