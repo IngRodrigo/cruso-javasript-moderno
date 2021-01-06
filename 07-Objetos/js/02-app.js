060ae930eec2f4c55a6b418f7e7273320478b01a
Rodrigo Jesus Sanchez <rodrigojesus-sanchez@aj.com.py>
2021-01-06 12:24:06 -0300

    acceder a propiedades de los objetos y sus valores

---
+++ 07-Objetos/js/02-app.js
@@ -0,0 +1,8 @@
+//acceder a propiedades de los objetos y sus propiedades
+
+const producto={
+    nombre:"Monitor de 20 pulgadas",
+    precio:200,
+    disponible:true
+}
+console.log(producto.nombre);
