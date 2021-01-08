e52c888174d4a176c4490a5bec3dd15f200d86dc
Rodrigo Jesus Sanchez <rodrigojesus-sanchez@aj.com.py>
2021-01-08 15:21:44 -0300

    Iniciando proyecto - validando formulario

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -2,6 +2,7 @@
 const email=document.querySelector('#email');
 const asunto=document.querySelector('#asunto');
 const mensaje=document.querySelector('#mensaje');
+const formulario=document.querySelector('#enviar-mail');
 //variables
 EventListener();
 function EventListener(){
@@ -21,5 +22,27 @@ function validarCampos(e){
         console.log(`hay datos`)
    }else{
        e.target.classList.add('border', 'border-red-500');
+       mostrarError('Todos los campos son obligatorios');
    }
+   //validar si el campo es email
+   if(e.target.type==='email'){
+        const email=e.target.value.indexOf('@');
+        if(email<0){
+            mostrarError('El email no es valido');
+        }
+   }
+}
+
+function mostrarError(mensaje){
+    //creamos el parrafo con el contenido del error;
+    const mensajeError=document.createElement('p');
+    mensajeError.textContent=mensaje;
+    //le damos unas clases para darles estilos
+    mensajeError.classList.add('border', 'border-red-500', 'background-red-100', 'text-red-500', 'p-3', 'mt-5', 'text-center', 'error');
+   //si buscando en todo el documento la clase error no existe
+    if(document.querySelectorAll('.error').length===0){
+        //aparece el mensaje de lo contrario no asi solo se mostrara una vez el mensaje 
+        //aunque el error aparezca en todos los input
+        formulario.appendChild(mensajeError);
+    }
 }
