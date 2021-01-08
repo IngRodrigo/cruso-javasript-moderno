c067f923c3848308fac26847f89671b996c878ee
IngRodrigo <romero10rodrigo@gmail.com>
2021-01-07 21:10:40 -0300

    Agregando elementos al carrito html desde js

--- 15-PROYECTO-Carrito/js/app.js
+++ 15-PROYECTO-Carrito/js/app.js
@@ -1,11 +1,12 @@
 const carrito=document.getElementById('carrito');
 //contenido del carrito //seleccionamos el div y luego el tbody
-const contenedorCarrito=document.getElementById('lista-carrito tbody');
+const contenedorCarrito=document.querySelector('#lista-carrito tbody');
 //boton para vaciar carrito
 const btnVaciarCarrito=document.getElementById('vaciar-carrito');
 //Lista de cursos
 const listaCursos=document.getElementById('lista-cursos');
-
+//variable para guardar los cursos
+let articuloCarrito=[];//igualamos a un array vacio
 //establecer listenner
 establecerListener();
 function establecerListener(){
@@ -18,6 +19,7 @@ function establecerListener(){
 
 //funciones
 function agregarCurso(e){
+    console.log(contenedorCarrito)
     e.preventDefault();//prevenimos la accion por defecto del enlace
     //target.classList muestra las clases css que tiene el div selccionado y contains() verifica si una de ellas es agregar-carrito
     if(e.target.classList.contains('agregar-carrito')){//si lo es entonces cumple la condicion para agregar al carrito
@@ -36,7 +38,7 @@ function vaciarCarrito(){
 
 //leer datos del curso y convertilo a un objeto utilizable por js
 function leerDatosDelCuros(curso){
-console.log(curso);
+// console.log(curso);
     const infoCurso={
         imagen:curso.querySelector('img').src,
         titulo:curso.querySelector('h4').textContent,
@@ -45,5 +47,30 @@ console.log(curso);
         cantidad:1
     }
 
-    console.log(infoCurso)
+    articuloCarrito=[...articuloCarrito, infoCurso];//agregamos el curso al array para mostrar en el carrito
+    // console.log(articuloCarrito)
+    carritoHTML(articuloCarrito);
+}
+
+
+//crear html para mostrar articuloCarritos
+function carritoHTML(articuloCarrito){
+    //antes de agregar al carrito se limpia para que quede vacio el html
+    //limpiar html
+    limpiarHTML();
+
+    articuloCarrito.forEach((curso) => {
+        const fila=document.createElement('tr');
+        fila.innerHTML=`
+        <td>
+        ${curso.titulo}
+        </td>
+        `;
+
+        contenedorCarrito.appendChild(fila);
+    });
+}
+
+function limpiarHTML(){
+
 }
